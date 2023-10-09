3ab1999ddd5995aea3b0d389df1b93bef89d0096
= <george.bentes@gmail.com>
2023-10-09 11:00:43 -0300

    fix: variables

--- variables.tf
+++ variables.tf
@@ -1,12 +1,7 @@
 variable "ANOTHER_VAR" {
-  type = string
   description = "CNAME value from environment variable"
-  default = null
 }
 
 variable "CNAME_NAME" {
-  type = string
   description = "CNAME name from environment variable"
-  default = null
 }
-
