3dbc54da2c35bd09a74f1ad7d4e1f0a458bd1108
= <george.bentes@gmail.com>
2023-10-09 08:35:48 -0300

    add: default values for variables in terraform

--- variables.tf
+++ variables.tf
@@ -1,10 +1,12 @@
 variable "ANOTHER_VAR" {
   type = string
   description = "CNAME value from environment variable"
+  default = null
 }
 
 variable "CNAME_NAME" {
   type = string
   description = "CNAME name from environment variable"
+  default = null
 }
 
